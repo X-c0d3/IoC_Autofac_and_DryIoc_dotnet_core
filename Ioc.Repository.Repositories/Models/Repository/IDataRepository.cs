61cb43e7777dc74b9de31f3d4c7a21843689067e
X-c0d3z <giffyp@hotmail.com>
2018-11-27 22:22:35 +0700

    commit

---
+++ Ioc.Repository.Repositories/Models/Repository/IDataRepository.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace Ioc.Repository.Repositories.Models.Repository
+{
+    public interface IDataRepository<TEntity, TDto>
+    {
+        IEnumerable<TEntity> GetAll();
+        TEntity Get(long id);
+        TDto GetDto(long id);
+        void Add(TEntity entity);
+        void Update(TEntity entityToUpdate, TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
